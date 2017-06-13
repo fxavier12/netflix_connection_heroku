6a655ccb00ad7df571fa82b75f004cb70654108d
Francisco da Cruz XAvier <fxavier@alunos.utfpr.edu.br>
2017-06-13 15:11:11 -0300

    Create ConexaoJDBC.java

--- src/main/java/database/ConexaoJDBC.java
+++ src/main/java/database/ConexaoJDBC.java
@@ -10,13 +10,8 @@ import java.util.Properties;
 public class ConexaoJDBC {
     Connection c;
     public Connection ConectaBD() throws SQLException, ClassNotFoundException{
-        Class.forName("org.postgresql.Driver");
-        String url = "jdbc:postgresql://ec2-23-21-220-152.compute-1.amazonaws.com:5432/d2g5ka61g7jr8jt";
-        Properties props = new Properties();
-        props.setProperty("user","cerhbpdhvcekbp");
-        props.setProperty("password","a55b0317ddb4fb9128a2a3c103bcd6e6a2ec25e0b08d95dc50601f4f52fff32f");
-        props.setProperty("sslmode","require");
-        c = DriverManager.getConnection(url, props);
+         String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        c = DriverManager.getConnection(dbUrl);
     	System.out.println("banco de dados conectado!");
         return c;
     }
