8833d34c0d77382f64cef1de076a0b2a0da18479
Francisco da Cruz XAvier <fxavier@alunos.utfpr.edu.br>
2017-06-13 16:48:05 -0300

    Create tradutor.js

--- src/main/webapp/JS/tradutor.js
+++ src/main/webapp/JS/tradutor.js
@@ -5,7 +5,7 @@ function tradutor(){
    				var valor = idioma.options[idioma.selectedIndex].value;
    			    console.log("oi");
    			    if(valor=="PT"){
-   			    	window.location = "../br/index.html";
+   			    	window.location = "../us/index.html";
    			    }else{
    			    	window.location = "../us/index.html";
    			    }
