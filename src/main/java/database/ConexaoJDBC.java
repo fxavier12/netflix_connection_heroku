49e894d27c20f34a152a0a612e2997fb32ff6d76
Francisco da Cruz XAvier <fxavier@alunos.utfpr.edu.br>
2017-06-13 14:54:19 -0300

    Create ConexaoJDBC.java

--- src/main/java/database/ConexaoJDBC.java
+++ src/main/java/database/ConexaoJDBC.java
@@ -10,7 +10,7 @@ public class ConexaoJDBC {
     Connection c;
     public Connection ConectaBD() throws SQLException, ClassNotFoundException{
         Class.forName("org.postgresql.Driver");
-        c = DriverManager.getConnection("jdbc:postgresql://cerhbpdhvcekbp:a55b0317ddb4fb9128a2a3c103bcd6e6a2ec25e0b08d95dc50601f4f52fff32f@ec2-23-21-220-152.compute-1.amazonaws.com:5432/d2g5ka61g7jr8ja55b0317ddb4fb9128a2a3c103bcd6e6a2ec25e0b08d95dc50601f4f52fff32f");
+        c = DriverManager.getConnection("postgres://cerhbpdhvcekbp:a55b0317ddb4fb9128a2a3c103bcd6e6a2ec25e0b08d95dc50601f4f52fff32f@ec2-23-21-220-152.compute-1.amazonaws.com:5432/d2g5ka61g7jr8ja55b0317ddb4fb9128a2a3c103bcd6e6a2ec25e0b08d95dc50601f4f52fff32f");
     	System.out.println("banco de dados conectado!");
         return c;
     }
